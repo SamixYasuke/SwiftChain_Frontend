cf5ab46393eaf815ae0ce80aaa1b1a52575347c1
samad13 <yusufabdulsamad93@gmail.com>
2026-04-28 20:38:03 +0100

    hou

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './global.css';
 import CommandPalette from '@/components/ui/CommandPalette';
 import TopLoader from '@/components/ui/TopLoader';
 import ToastProvider from '@/components/providers/ToastProvider';
+import ModalProvider from '@/components/providers/ModalProvider';
 
 export const metadata = {
   title: 'SwiftChain',
@@ -16,11 +17,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ToastProvider>
-          <TopLoader />
-          <CommandPalette />
-          {children}
-        </ToastProvider>
+        <ModalProvider>
+          <ToastProvider>
+            <TopLoader />
+            <CommandPalette />
+            {children}
+          </ToastProvider>
+        </ModalProvider>
       </body>
     </html>
   );
